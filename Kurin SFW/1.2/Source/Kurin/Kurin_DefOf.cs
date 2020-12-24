d729768269af247b5efaf275a9e803ad4c5f095e
MaroonToon <62262328+MaroonToon@users.noreply.github.com>
2020-12-23 19:32:26 -0500

    Initial Upload

---
+++ Kurin SFW/1.2/Source/Kurin/Kurin_DefOf.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using AlienRace;
+using RimWorld;
+using Verse;
+
+namespace NineTailedFox
+{
+	[DefOf]
+	public static class Kurin_DefOf
+	{
+		/*public static ThingDef_AlienRace Kurin_Race;
+
+		public static ScenarioDef Kurin_Scenario;
+
+		public static FactionDef Kurin_Player;
+
+		public static FactionDef Kurin_KurinFaction;
+
+		public static PawnKindDef Kurin_Colonist;
+
+		public static PawnKindDef Kurin_Citizen;
+
+		public static PawnKindDef Kurin_Vigilante;
+
+		public static PawnKindDef Kurin_Trader;
+
+		public static PawnKindDef Kurin_Elite;
+
+		public static PawnKindDef Kurin_Soldier;
+
+		public static PawnKindDef Kurin_EliteSoldier;
+
+		public static PawnKindDef Kurin_Sniper;
+*/
+		public static HediffDef Kurin_NooseTrapHediff;
+	}
+}
